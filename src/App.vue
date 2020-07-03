9c202bfb7ac1a5cf269a2bb4138c6b4faf688186
liuxv <1721518931@qq.com>
2020-07-03 11:29:04 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {}
+};
+</script>
+
+<style>
+@import "~@/assets/styles/reset.css";
+@import "~@/assets/styles/border.css";
+@import "~@/assets/styles/iconfont.css";
+</style>
---
+++ src/vue.config.js
@@ -0,0 +1,7 @@
+export const configWebpack = {
+  resolve: {
+    alias: {
+      'src': '@'
+    }
+  }
+}
